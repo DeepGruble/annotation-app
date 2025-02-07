04a13cb38525ada528931c8e5811da62ec215dd8
mmmmaja <mj.gojska@gmail.com>
2025-02-07 13:34:04 +0100

    change the main page

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from enum import Enum
 import os
 import matplotlib.pyplot as plt
-from PIL import Image
-import random
-import numpy as np
-from model import load_sam2
-from model import ModelSize
+from SAM2_model import load_sam2
+from SAM2_model import ModelSize
+from segmentation_utils import *
 
 
-# Color palette:
+# Color palette for the UI:
 # https://coolors.co/b74f6f-adbdff-3185fc-34e5ff-35281d
-
-# ===============================================================
-# Constants
-# ===============================================================
 
 # Path to the dataset
 IMAGE_PATH = "C:\\Users\\MajaGojska\\Desktop\\classification_dataset_split\\train"
 # Where the segmentation masks will be saved
 SEGMENTATION_MASKS_PATH = "C:\\Users\\MajaGojska\\Desktop\\classification_dataset_masks\\train"
+
 
 class ImgeType(Enum):
     BITEWING = "bitewing"
@@ -28,10 +22,12 @@ class ImgeType(Enum):
     COLOR_ZOOM = "color_zoom"
     PANORAMIC = "panoramic"
     PERIAPICAL = "periapical"
+    
 
-# ===============================================================
-# Initialize the session state
-# ===============================================================
+# TODO: This value depends on the page entered by the user
+IMAGE_TYPE = ImgeType.COLOR_PANORAMIC
+
+
 def read_images(image_type=ImgeType.PANORAMIC):
     """
     Read images from the specified directory.
@@ -45,7 +41,9 @@ def read_images(image_type=ImgeType.PANORAMIC):
         )
     return images
 
+# Initialize the session state
 if "images" not in st.session_state:
+    # Read the images from the dataset
     st.session_state.images = read_images()
     st.session_state.current_image_index = 0
     st.session_state.prompts = []
@@ -67,35 +65,11 @@ def get_current_image():
         return None
 
 
-def process_clicks(points, image):
-    image_np = np.array(image)
-    st.session_state.model.set_image(image_np)
-    
-    # Prepare the prompt
-    input_point = np.array(points)
-    input_label = np.array([1] * len(points))
-    
-    # Generate the segmentation mask
-    masks, _, _ = st.session_state.model.predict(input_point, input_label)
-    return masks[0]
-
-
-def overlay_mask(image, mask, color):
-    """
-    Overlay the mask on the image.
-    :param image: The image to overlay the mask on
-    :param mask: The mask to overlay (binary mask)
-    :param color: The color to use for the overlay
-    """
-    image_np = np.array(image)
-    overlayed_image = image_np.copy()
-    overlayed_image[mask == 1] = color
-    return Image.fromarray(overlayed_image) 
 
 # ===============================================================
 # Streamlit UI
 # ===============================================================
-st.set_page_config(layout="wide", page_title="Object Segmentation with SAM2")
+st.set_page_config(layout="wide", page_title=f"Numbering Tool - {IMAGE_TYPE.value}")
 
 # Custom CSS for styling: buttons, progress bar, and headers
 st.markdown(
@@ -130,12 +104,6 @@ st.markdown(
 
 # Sidebar for controls
 with st.sidebar:
-    st.header("Settings")
-    image_type = st.selectbox(
-        "Select Image Type",
-        [img_type.value for img_type in ImgeType],
-        index=3,  # Default to PANORAMIC
-    )
 
     # Tooth numbers and their corresponding colors
     tooth_colormap = plt.get_cmap("hsv", 32)
@@ -148,23 +116,18 @@ with st.sidebar:
         index=0,  # Default to tooth number 1
     )
     st.session_state.tooth_number = tooth_number
-
-    if st.button("Reload Images"):
-        st.session_state.images = read_images(ImgeType(image_type))
-        st.session_state.current_image_index = 0
-        st.session_state.prompt = None
-        st.session_state.mask = None
+        
+    stroke_width = st.slider("Stroke Width", 1, 10, 2)
+    
 
 # Main content
-st.title("Object Segmentation with SAM2")
-st.markdown("Draw on the image to generate a segmentation mask.")
+st.title("Numbering Tool")
 
 # Display current image and progress
 current_image = get_current_image()
 if current_image is not None:
     col1, col2 = st.columns([3, 1])
     with col1:
-        st.markdown("### Current Image")
         RESIZE_FACTOR = 2.5
         current_image = current_image.resize((int(224 * RESIZE_FACTOR), int(224 * RESIZE_FACTOR)))
 
@@ -177,7 +140,7 @@ if current_image is not None:
         # Display the canvas with the overlayed image
         canvas_result = st_canvas(
             fill_color="",
-            stroke_width=4,
+            stroke_width=stroke_width,
             background_color="#FFFFFF",
             update_streamlit=True,
             stroke_color="red",
@@ -191,18 +154,13 @@ if current_image is not None:
         col1a, col1b, col1c = st.columns(3)
         with col1a:
             if st.button("Show Mask"):
+                # FIXME
                 st.session_state.mask = process_clicks(st.session_state.prompts, current_image)
                 st.rerun()
 
         with col1b:
             if st.button("Save Mask"):
                 pass  # Add saving logic here
-
-        with col1c:
-            if st.button("Clear Mask"):
-                st.session_state.prompts = []
-                st.session_state.mask = None
-                st.rerun()
             
     with col2:
         st.markdown("### Progress")
