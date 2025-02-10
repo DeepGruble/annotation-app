dd06170d0439cf5758284186d3af6c3a9847fbeb
mmmmaja <mj.gojska@gmail.com>
2025-02-10 14:00:10 +0100

    switch to bboxes

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from streamlit_drawable_canvas import st_canvas
 from enum import Enum
 import os
 import matplotlib.pyplot as plt
-from SAM2_model import load_sam2
-from SAM2_model import ModelSize
-from segmentation_utils import *
+import streamlit as st
+from PIL import Image
+
 
 
 class ImgeType(Enum):
@@ -14,11 +14,27 @@ class ImgeType(Enum):
     PANORAMIC = "panoramic"
     PERIAPICAL = "periapical"
     
-
+    
 # TODO: This value depends on the page entered by the user
 IMAGE_TYPE = ImgeType.COLOR_PANORAMIC
 
 st.set_page_config(layout="wide", page_title=f"Numbering Tool - {IMAGE_TYPE.value}")
+
+# Read the css style template
+css_path = os.path.join(os.path.dirname(__file__), "style_template.css")
+if os.path.exists(css_path):
+    with open(css_path, "r") as f:
+        css_template = f.read()
+    css = css_template
+    st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+else:
+    st.warning("CSS template not found. Please check the path.")
+
+
+
+    
+
+
 
 
 # Color palette for the UI:
@@ -26,8 +42,10 @@ st.set_page_config(layout="wide", page_title=f"Numbering Tool - {IMAGE_TYPE.valu
 
 # Path to the dataset
 IMAGE_PATH = "C:\\Users\\MajaGojska\\Desktop\\classification_dataset_split\\train"
-# Where the segmentation masks will be saved
-SEGMENTATION_MASKS_PATH = "C:\\Users\\MajaGojska\\Desktop\\classification_dataset_masks\\train"
+# Where the labels are stored
+LABEL_PATH = "C:\\Users\\MajaGojska\\Desktop\\classification_dataset_labels\\train"
+if not os.path.exists(LABEL_PATH):
+    os.makedirs(LABEL_PATH)
 
 
 def read_images(image_type=ImgeType.PANORAMIC):
@@ -48,13 +66,8 @@ if "images" not in st.session_state:
     # Read the images from the dataset
     st.session_state.images = read_images()
     st.session_state.current_image_index = 0
-    st.session_state.prompts = []
-    st.session_state.mask = None
+    st.session_state.labels = []
     st.session_state.tooth_number = 1  # Default tooth number
-    
-    # Initialize the SAM2 model
-    st.session_state.model = load_sam2(model_size=ModelSize.TINY)
-    print("Model initialized successfully")
     
 
 def get_current_image():
@@ -65,53 +78,24 @@ def get_current_image():
         return st.session_state.images[st.session_state.current_image_index]
     else:
         return None
-
+    
+def rgb_to_hex(rgb):
+    """
+    Convert an RGB tuple to a hex color string.
+    """
+    return '#%02x%02x%02x' % (int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255))
 
 
 # ===============================================================
 # Streamlit UI
 # ===============================================================
 
-# Custom CSS for styling: buttons, progress bar, and headers
-st.markdown(
-    """
-    <style>
-    
-    /* Main style button */
-    .stButton>button {
-        background-color: #ADBDFF;
-        color: white;
-        border-radius: 5px;
-        padding: 10px 20px;
-        font-size: 10px;
-        border: 1px solid #ADBDFF;
-    }
-    .stButton>button:hover {
-        background-color: #3185fc;
-        border: 1px solid #3185fc;
-        color: white;
-    }
-    .stButton>button:active {
-        background-color: #3185fc;
-        border: 1px solid #3185fc;
-        color: white;
-    }
-    
-    /* Tooth selection buttons */
-    .tooth-buttons {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        
-    }
-    
-    """,
-    unsafe_allow_html=True,
-)
-
 # Sidebar for controls
 with st.sidebar:
+    
+    st.markdown("### Progress")
+    st.progress((st.session_state.current_image_index + 1) / len(st.session_state.images))
+    st.write(f"Image {st.session_state.current_image_index + 1} of {len(st.session_state.images)}")
 
     # Tooth selection
     # Tooth numbers and their corresponding colors
@@ -125,34 +109,6 @@ with st.sidebar:
     
     st.markdown('<div class="tooth-buttons">', unsafe_allow_html=True)
     
-    st.markdown("""
-        <style>
-        
-        .element-container:has(#button-after) + div button {
-            background-color: transparent;
-            font-size: 0.5em !important;
-            color: white;
-            border: 1px solid #adbdff;
-            height: 10px;
-            width: 30px;
-            0px;
-            border-radius: 3px;
-            padding: 2px;
-            cursor: pointer;
-        }
-        .element-container:has(#button-after) + div button:hover {
-            border: 1px solid #3185FC;
-        }
-        .element-container:has(#button-after) + div button:active {
-            background-color: #3185FC;
-        }
-        .element-container:has(#button-after) + div button:focus {
-            background-color: #3185FC;
-        }
-        
-        </style>""", 
-        unsafe_allow_html=True)
-    
     cols = st.columns(16)
     for i, tooth in enumerate(list(range(1, 33))):
         with cols[i % 16]:
@@ -165,65 +121,70 @@ with st.sidebar:
                 st.rerun()        
     st.markdown('</div>', unsafe_allow_html=True)  # Close the div wrapper
     
+    # Add the zoom slider
+    zoom = st.slider("Zoom", min_value=1., max_value=8., value=1., step=0.1)
 
-    stroke_width = st.slider("Stroke Width", 1, 10, 2)
-    
+
 
 # Main content
-st.title("Numbering Annotation Tool")
+#st.title("Numbering Annotation Tool")
 
 # Display current image and progress
 current_image = get_current_image()
 if current_image is not None:
-    col1, col2 = st.columns([3, 1])
-    with col1:
-        RESIZE_FACTOR = 2.5
-        current_image = current_image.resize((int(224 * RESIZE_FACTOR), int(224 * RESIZE_FACTOR)))
 
-        # Overlay the mask on the current image if it exists
-        if st.session_state.mask is not None:
-            overlayed_image = overlay_mask(current_image, st.session_state.mask, color=tooth_colors_dict[st.session_state.tooth_number])
-        else:
-            overlayed_image = current_image
-
-        # Display the canvas with the overlayed image
-        canvas_result = st_canvas(
-            fill_color="",
-            stroke_width=stroke_width,
-            background_color="#FFFFFF",
-            update_streamlit=True,
-            stroke_color="red",
-            background_image=overlayed_image,
-            width=current_image.size[0],
-            height=current_image.size[1],
-            drawing_mode="circle",
-            key="canvas",
-        )
         
-        col1a, col1b, col1c = st.columns(3)
-        with col1a:
-            if st.button("Show Mask", type="primary"):
-                st.session_state.mask = process_clicks(st.session_state.prompts, current_image)
-                st.rerun()
+    width = int(current_image.width * zoom)
+    height = int(current_image.height * zoom)
+    print(width, height)
 
-        with col1b:
-            if st.button("Save Mask", type="primary"):
-                pass  # Add saving logic here
+    # Display the canvas with the overlayed image
+    canvas_result = st_canvas(
+        fill_color="",
+        stroke_width=2,
+        background_color="#FFFFFF",
+        update_streamlit=True,
+        stroke_color=rgb_to_hex(tooth_colors_dict[st.session_state.tooth_number]),
+        background_image=current_image,
+        width=width * 2,
+        height=height * 2,
+        drawing_mode="rect",
+        key="canvas",
+    )
+    
+    column_1, column_2 = st.columns(2)
+    with column_1:
+        if st.button("Submit", type="primary"):
+            pass
+
+    with column_2:
+        if st.button("Skip", type="primary"):
+            pass  # Add saving logic here
             
-    with col2:
-        st.markdown("### Progress")
-        st.progress((st.session_state.current_image_index + 1) / len(st.session_state.images))
-        st.write(f"Image {st.session_state.current_image_index + 1} of {len(st.session_state.images)}")
+        
 
     # Handle canvas result
     if canvas_result and canvas_result.json_data is not None:
         objects = canvas_result.json_data["objects"]
         if objects:
             last_object = objects[-1]
-            x = last_object["left"] + last_object["radius"]
-            y = last_object["top"] + last_object["radius"]
-            if (x, y) not in st.session_state.prompts:
-                st.session_state.prompts.append((x, y))
+            
+            height = last_object["height"]
+            width = last_object["width"]
+            
+            left = last_object["left"]
+            top = last_object["top"]
+            
+            label = {
+                "tooth_number": st.session_state.tooth_number,
+                "left": left,
+                "top": top,
+                "width": width,
+                "height": height,
+            }
+            print(label)
+            if label not in st.session_state.labels:
+               st.session_state.labels.append(label)
                 
 else:
     st.warning("No images to display. Please check the image directory.")
