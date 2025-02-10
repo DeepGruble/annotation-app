251a0cec21731ffdc112f590b1996cad1d5b8a47
mmmmaja <mj.gojska@gmail.com>
2025-02-10 11:24:53 +0100

    add tooth selection buttons

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ from SAM2_model import load_sam2
 from SAM2_model import ModelSize
 from segmentation_utils import *
 
-
-# Color palette for the UI:
-# https://coolors.co/b74f6f-adbdff-3185fc-34e5ff-35281d
-
-# Path to the dataset
-IMAGE_PATH = "C:\\Users\\MajaGojska\\Desktop\\classification_dataset_split\\train"
-# Where the segmentation masks will be saved
-SEGMENTATION_MASKS_PATH = "C:\\Users\\MajaGojska\\Desktop\\classification_dataset_masks\\train"
-
-
 class ImgeType(Enum):
     BITEWING = "bitewing"
     COLOR_PANORAMIC = "color_panoramic"
@@ -26,6 +16,17 @@ class ImgeType(Enum):
 
 # TODO: This value depends on the page entered by the user
 IMAGE_TYPE = ImgeType.COLOR_PANORAMIC
+
+st.set_page_config(layout="wide", page_title=f"Numbering Tool - {IMAGE_TYPE.value}")
+
+
+# Color palette for the UI:
+# https://coolors.co/b74f6f-adbdff-3185fc-34e5ff-35281d
+
+# Path to the dataset
+IMAGE_PATH = "C:\\Users\\MajaGojska\\Desktop\\classification_dataset_split\\train"
+# Where the segmentation masks will be saved
+SEGMENTATION_MASKS_PATH = "C:\\Users\\MajaGojska\\Desktop\\classification_dataset_masks\\train"
 
 
 def read_images(image_type=ImgeType.PANORAMIC):
@@ -51,9 +52,9 @@ if "images" not in st.session_state:
     st.session_state.tooth_number = 1  # Default tooth number
     
     # Initialize the SAM2 model
-    st.session_state.model = load_sam2(model_size=ModelSize.LARGE)
+    st.session_state.model = load_sam2(model_size=ModelSize.TINY)
     print("Model initialized successfully")
-
+    
 
 def get_current_image():
     """
@@ -69,33 +70,47 @@ def get_current_image():
 # ===============================================================
 # Streamlit UI
 # ===============================================================
-st.set_page_config(layout="wide", page_title=f"Numbering Tool - {IMAGE_TYPE.value}")
 
 # Custom CSS for styling: buttons, progress bar, and headers
 st.markdown(
     """
     <style>
-    .stButton button {
+    
+    /* Main style button */
+    .stButton>button {
         background-color: #ADBDFF;
         color: white;
         border-radius: 5px;
         padding: 10px 20px;
-        font-size: 16px;
+        font-size: 10px;
         border: 1px solid #ADBDFF;
     }
-    .stButton button:hover {
+    .stButton>button:hover {
         background-color: #3185fc;
-        color: white;
-        border-radius: 5px;
-        padding: 10px 20px;
-        font-size: 16px;
         border: 1px solid #3185fc;
+        color: white;
     }
-    .stProgress > div > div > div {
-        background-color: #ADBDFF;
+    .stButton>button:active {
+        background-color: #3185fc;
+        border: 1px solid #3185fc;
+        color: white;
     }
-    .stMarkdown h1 {
-        color: #4CAF50;
+    
+    /* Tooth selection buttons */
+    .tooth-buttons {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        
+    }
+    .tooth-buttons .stButton>button {
+        background-color:rgb(0, 0, 0) !important;
+        color: black;
+        border-radius: 5px;
+        padding: 5px 10px;
+        margin: 5px;
+        border: 1px solid #ADBDFF;
     }
     </style>
     """,
@@ -105,18 +120,48 @@ st.markdown(
 # Sidebar for controls
 with st.sidebar:
 
+    # Tooth selection
     # Tooth numbers and their corresponding colors
     tooth_colormap = plt.get_cmap("hsv", 32)
-    TOOTH_COLORS = {i + 1: tooth_colormap(i / 32)[:3] for i in range(32)}
+    # Dictionary mapping tooth numbers to colors
+    tooth_colors_dict = {i + 1: tooth_colormap(i / 32)[:3] for i in range(32)}
+    
+    #  Create a grid of buttons for tooth selection
+    st.markdown("### Tooth Selection")
+    
+    st.markdown('<div class="tooth-buttons">', unsafe_allow_html=True)
+    
+    st.markdown("""
+        <style>.element-container:has(#button-after) + div button {
+            background-color: transparent;
+            font-size: 8px !important;
+            color: white;
+            border: 1px solid ##34E5FF;
+            height: 10px;
+            width: 30px;
+            0px;
+            border-radius: 3px;
+            padding: 2px;
+            cursor: pointer;
             
-    # Selectbox for tooth number
-    tooth_number = st.selectbox(
-        "Select Tooth Number",
-        list(TOOTH_COLORS.keys()),
-        index=0,  # Default to tooth number 1
-    )
-    st.session_state.tooth_number = tooth_number
-        
+            
+            
+        }</style>""", 
+        unsafe_allow_html=True)
+    
+    cols = st.columns(16)
+    for i, tooth in enumerate(list(range(1, 33))):
+        with cols[i % 16]:
+            color = tooth_colors_dict[tooth]
+            st.markdown('<span id="button-after"></span>', unsafe_allow_html=True)
+ 
+            if st.button(f"{tooth}", key=f"button{tooth}"):
+                st.session_state.tooth_number = tooth
+                print(f"Tooth {tooth} selected")
+                st.rerun()        
+    st.markdown('</div>', unsafe_allow_html=True)  # Close the div wrapper
+    
+
     stroke_width = st.slider("Stroke Width", 1, 10, 2)
     
 
@@ -133,7 +178,7 @@ if current_image is not None:
 
         # Overlay the mask on the current image if it exists
         if st.session_state.mask is not None:
-            overlayed_image = overlay_mask(current_image, st.session_state.mask, TOOTH_COLORS[st.session_state.tooth_number])
+            overlayed_image = overlay_mask(current_image, st.session_state.mask, color=tooth_colors_dict[st.session_state.tooth_number])
         else:
             overlayed_image = current_image
 
@@ -153,13 +198,12 @@ if current_image is not None:
         
         col1a, col1b, col1c = st.columns(3)
         with col1a:
-            if st.button("Show Mask"):
-                # FIXME
+            if st.button("Show Mask", type="primary"):
                 st.session_state.mask = process_clicks(st.session_state.prompts, current_image)
                 st.rerun()
 
         with col1b:
-            if st.button("Save Mask"):
+            if st.button("Save Mask", type="primary"):
                 pass  # Add saving logic here
             
     with col2:
